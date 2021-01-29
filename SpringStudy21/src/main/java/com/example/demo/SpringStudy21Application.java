403360423cb09cb707987773bbc5741f026227d0
AIkaKon <ika09861@gmail.com>
2021-01-30 03:58:13 +0900

    初回コミット

---
+++ SpringStudy21/src/main/java/com/example/demo/SpringStudy21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy21Application.class, args);
+	}
+
+}
---
+++ SpringStudy23/src/main/java/com/example/demo/SpringStudy23Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudy23Application.class, args);
+	}
+
+}
